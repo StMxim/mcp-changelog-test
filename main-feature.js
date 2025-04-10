5495f2a995b51ccc41e5f3d16a624bfb179479eb
maximdoc <maximdoc2000@gmail.com>
2025-04-10 11:27:03 +0300

    Добавлена функциональность в ветке main

---
+++ main-feature.js
@@ -0,0 +1,12 @@
+/**
+ * Файл, созданный в ветке main
+ */
+
+function mainFunction() {
+  console.log('Это функция в ветке main');
+  return 'Main функция';
+}
+
+module.exports = {
+  mainFunction
+}; 
